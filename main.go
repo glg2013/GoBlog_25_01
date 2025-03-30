b5b95e4ada58d7704d9421701b1ca4a9189ee43b
fairy_shrimp <fengniancong@163.com>
2025-03-30 15:58:23 +0800

    新建博文表单

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"strings"
 )
 
+var router = mux.NewRouter()
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Hello,欢迎来到 goblog</h1>")
 }
@@ -55,8 +57,28 @@ func removeTrailingSlash(next http.Handler) http.Handler {
 	})
 }
 
+func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
+	html := `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>创建文章 - 我的技术博客</title>
+</head>
+<body>
+    <form action="%s" method="POST">
+        <p><input type="text" name="title" placeholder="文章标题"><p>
+        <p><textarea name="body" placeholder="文章内容"></textarea><p>
+        <p><button type="submit">发布文章</button><p>
+    </form>
+</body>
+</html>
+`
+	storeURL, _ := router.Get("articles.store").URL()
+	fmt.Println(storeURL, " 看看这里获取的url 是什么")
+	fmt.Fprintf(w, html, storeURL)
+}
+
 func main() {
-	router := mux.NewRouter()
 
 	router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
 
@@ -66,18 +88,21 @@ func main() {
 	router.HandleFunc("/articles", articlesIndexHandler).Methods("GET").Name("articles.index")
 	router.HandleFunc("/articles", articlesStoreHandler).Methods("POST").Name("articles.store")
 
+	// 创建博客相关路由
+	router.HandleFunc("/articles/create", articlesCreateHandler).Methods("GET").Name("articles.create")
+
 	// 自定义 404 页面
 	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
 
 	// 中间件：强制内容类型为 HTML
 	router.Use(forceHTMLMiddleware)
 
-	// 通过命名路由获取 URL 示例
-	homeURL, _ := router.Get("home").URL()
-	fmt.Println("homeURL: ", homeURL)
-
-	articleURL, _ := router.Get("articles.show").URL("id", "23")
-	fmt.Println("articleURL: ", articleURL)
+	//// 通过命名路由获取 URL 示例
+	//homeURL, _ := router.Get("home").URL()
+	//fmt.Println("homeURL: ", homeURL)
+	//
+	//articleURL, _ := router.Get("articles.show").URL("id", "23")
+	//fmt.Println("articleURL: ", articleURL)
 
 	http.ListenAndServe(":3000", removeTrailingSlash(router))
 
