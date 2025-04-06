1fcab289565bcaed3a97c813aa53e29fd6c180ca
fairy_shrimp <fengniancong@163.com>
2025-04-06 19:58:34 +0800

    配置和环境变量

--- pkg/model/model.go
+++ pkg/model/model.go
@@ -2,6 +2,8 @@
 package model
 
 import (
+	"fmt"
+	"goblog/pkg/config"
 	"goblog/pkg/logger"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -15,13 +17,29 @@ func ConnectDB() *gorm.DB {
 
 	var err error
 
-	config := mysql.New(mysql.Config{
-		DSN: "homestead:secret@tcp(192.168.56.56:3306)/goblog?charset=utf8&parseTime=True&loc=Local",
+	// 初始化 MySQL 连接信息
+	gormConfig := mysql.New(mysql.Config{
+		DSN: fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=%v&parseTime=True&loc=Local",
+			config.GetString("database.mysql.username"),
+			config.GetString("database.mysql.password"),
+			config.GetString("database.mysql.host"),
+			config.GetString("database.mysql.port"),
+			config.GetString("database.mysql.database"),
+			config.GetString("database.mysql.charset")),
 	})
 
+	var level gormlogger.LogLevel
+	if config.GetBool("app.debug") {
+		// 读取不到数据也会显示
+		level = gormlogger.Warn
+	} else {
+		// 只有错误才会显示
+		level = gormlogger.Error
+	}
+
 	// 准备数据库连接池
-	DB, err = gorm.Open(config, &gorm.Config{
-		Logger: gormlogger.Default.LogMode(gormlogger.Warn),
+	DB, err = gorm.Open(gormConfig, &gorm.Config{
+		Logger: gormlogger.Default.LogMode(level),
 	})
 
 	logger.LogError(err)
