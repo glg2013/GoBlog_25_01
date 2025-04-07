954e1dff68f1af7886dadcd8a70f848a4ba242d4
gaofeng <57412960@qq.com>
2025-04-07 11:09:50 +0800

    文章作者

--- app/models/article/article.go
+++ app/models/article/article.go
@@ -3,6 +3,7 @@ package article
 
 import (
 	"goblog/app/models"
+	"goblog/app/models/user"
 	"goblog/pkg/route"
 	"strconv"
 )
@@ -13,9 +14,16 @@ type Article struct {
 
 	Title string `gorm:"type:varchar(255);not null" valid:"title"`
 	Body  string `gorm:"type:longtext;not null" valid:"body"`
+
+	UserID uint64 `gorm:"not null;index"`
+	User   user.User
 }
 
 // Link 方法用来生成文章链接
 func (a Article) Link() string {
 	return route.Name2URL("articles.show", "id", strconv.FormatUint(a.ID, 10))
+}
+
+func (a Article) CreatedAtDate() string {
+	return a.CreatedAt.Format("2006-01-02")
 }
